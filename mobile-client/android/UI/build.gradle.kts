f91dc06631d559962b1ea1222c54b4a8356feb28
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 01:21:43 -0500

    materialize Android UI layer

---
+++ mobile-client/android/UI/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.smartresponsor.mobile.client.ui"
+    compileSdk = 34
+    defaultConfig { minSdk = 24 }
+}
