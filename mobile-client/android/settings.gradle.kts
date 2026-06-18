9856fea516557eefe96bb6e9277c1de3a542ca71
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 01:22:34 -0500

    materialize Android module graph

--- mobile-client/android/settings.gradle.kts
+++ mobile-client/android/settings.gradle.kts
@@ -1,2 +1,38 @@
-rootProject.name = "Mobile"
-include(":app", ":core:config", ":core:entitlement", ":core:billing", ":core:analytic", ":core:push", ":core:security")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mobiling"
+
+include(
+    ":app",
+    ":client-contract",
+    ":client-data",
+    ":client-usecase",
+    ":client-navigation",
+    ":client-ui",
+    ":core:config",
+    ":core:entitlement",
+    ":core:billing",
+    ":core:analytic",
+    ":core:push",
+    ":core:security",
+)
+
+project(":client-contract").projectDir = file("Contract")
+project(":client-data").projectDir = file("Data")
+project(":client-usecase").projectDir = file("UseCase")
+project(":client-navigation").projectDir = file("Navigation")
+project(":client-ui").projectDir = file("UI")
