ac721409bad4b102168e40342315ec665bd8c9ef
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 01:19:25 -0500

    materialize Android UseCase module

---
+++ mobile-client/android/UseCase/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.smartresponsor.mobile.client.usecase"
+    compileSdk = 34
+    defaultConfig { minSdk = 24 }
+    sourceSets["main"].java.srcDirs(".")
+}
+
+dependencies {
+    implementation(project(":client-contract"))
+    implementation(project(":client-data"))
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
