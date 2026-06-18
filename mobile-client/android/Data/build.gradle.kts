0cc9a695739ae68fc86b8c74e14945450e9df25a
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-06-18 01:19:17 -0500

    complete Android Data module dependencies

--- mobile-client/android/Data/build.gradle.kts
+++ mobile-client/android/Data/build.gradle.kts
@@ -12,4 +12,8 @@ android {
 
 dependencies {
     implementation(project(":client-contract"))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
