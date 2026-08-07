82ad2620737d5a9cd897bf4468855a889b170cc6
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-08-06 23:37:55 -0500

    Fix Android catalog layout import

--- client/android/app/src/main/java/app/mobiling/client/catalog/CatalogMobileScreen.kt
+++ client/android/app/src/main/java/app/mobiling/client/catalog/CatalogMobileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
